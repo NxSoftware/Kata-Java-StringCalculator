f5cd743ab9b62f62cf9886b217068cf15d89b1a6
Steve Wilford <steve@stevewilford.co.uk>
2017-01-25 18:06:21 +0000

    Parse integer using built-in method

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -6,24 +6,18 @@ public class StringCalculator {
     public static int Add(String s)
     {
         int number = 0;
+
+        if (s.length() == 0)
+        {
+            return number;
+        }
+
         String[] digitStrings = s.split(",");
         for (String digitString : digitStrings)
         {
-            number += getInteger(digitString);
+            number += Integer.parseInt(digitString);
         }
         return number;
-    }
-
-    private static int getInteger(String s)
-    {
-        for (Integer num = 0; num < 9; num++)
-        {
-            if (s.equals(num.toString()))
-            {
-                return num;
-            }
-        }
-        return 0;
     }
 
 }
