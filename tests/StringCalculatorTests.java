28c867db4786c8aabdaf1a5b54f4bf163fcc8d75
Steve Wilford <steve@stevewilford.co.uk>
2017-01-25 18:01:10 +0000

    Add additional two digit test cases

--- tests/StringCalculatorTests.java
+++ tests/StringCalculatorTests.java
@@ -24,6 +24,10 @@ public class StringCalculatorTests {
         assertEquals(1, StringCalculator.Add("0,1"));
         assertEquals(2, StringCalculator.Add("0,2"));
         assertEquals(2, StringCalculator.Add("1,1"));
+        assertEquals(3, StringCalculator.Add("1,2"));
+        assertEquals(3, StringCalculator.Add("2,1"));
+        assertEquals(4, StringCalculator.Add("2,2"));
+        assertEquals(5, StringCalculator.Add("3,2"));
     }
 
 }
