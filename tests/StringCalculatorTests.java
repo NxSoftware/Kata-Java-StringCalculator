fa368edae309998584bd1b51b40c1620b893f7ec
Steve Wilford <steve@stevewilford.co.uk>
2017-01-28 14:55:21 +0000

    Add tests for four digits

--- tests/StringCalculatorTests.java
+++ tests/StringCalculatorTests.java
@@ -43,4 +43,14 @@ public class StringCalculatorTests {
         assertEquals(3, StringCalculator.Add("1,1,1"));
     }
 
+    @Test
+    void testFourDigits()
+    {
+        assertEquals(0, StringCalculator.Add("0,0,0,0"));
+        assertEquals(1, StringCalculator.Add("0,0,0,1"));
+        assertEquals(2, StringCalculator.Add("0,0,1,1"));
+        assertEquals(3, StringCalculator.Add("0,1,1,1"));
+        assertEquals(4, StringCalculator.Add("1,1,1,1"));
+    }
+
 }
